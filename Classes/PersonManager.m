ff312e3b500bd041acaa1ad40863bbc58ab6b5ee
Takuya Murakami <tmurakam@tmurakam.org>
2009-04-29 23:50:48 +0900

    add test data

--- Classes/PersonManager.m
+++ Classes/PersonManager.m
@@ -24,11 +24,18 @@
     persons = [[NSMutableArray alloc] init];
 
     // test data
-    Person *p = [[[Person alloc] init] autorelease];
-    p.name = @"テストユーザ";
+    Person *p;
+    p = [[[Person alloc] init] autorelease];
+    p.name = @"作者";
     [p setDate:[[[SimpleDate alloc] initWithYear:1970 month:1 day:1] autorelease] type:EV_AGE];
-    [p setDate:[[[SimpleDate alloc] initWithYear:2000 month:1 day:1] autorelease] type:EV_MARRIAGE];
+    [p setDate:[[[SimpleDate alloc] initWithYear:2005 month:1 day:1] autorelease] type:EV_MARRIAGE];
+    [persons addObject:p];
 
+    p = [[[Person alloc] init] autorelease];
+    p.name = @"父";
+    [p setDate:[[[SimpleDate alloc] initWithYear:1944 month:1 day:1] autorelease] type:EV_AGE];
+    [p setDate:[[[SimpleDate alloc] initWithYear:1970 month:1 day:1] autorelease] type:EV_MARRIAGE];
+    [p setDate:[[[SimpleDate alloc] initWithYear:1990 month:1 day:1] autorelease] type:EV_DEATH];
     [persons addObject:p];
 
     return self;

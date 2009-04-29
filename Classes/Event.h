ee125caac710b83a94489440c9f2e743fd38211b
Takuya Murakami <tmurakam@tmurakam.org>
2009-04-29 18:43:39 +0900

    updated

--- Classes/Event.h
+++ Classes/Event.h
@@ -1,6 +1,12 @@
 // -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
 
-// person.h
+// Event.h
+
+#import <UIKit/UIKit.h>
+
+#define EV_AGE      0 // 年齢イベント
+#define EV_MARRIAGE 1 // 結婚イベント
+#define EV_DEATH    2 // 法要イベント
 
 @interface Event : NSObject {
     int years;  // 経過年

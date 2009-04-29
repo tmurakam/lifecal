98d1668dbcfb8730fa503ff4042f6b9bc3dca8cf
Takuya Murakami <tmurakam@tmurakam.org>
2009-04-29 16:30:02 +0900

    first commit

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  lifecal
+//
+//  Created by 村上 卓弥 on 09/04/29.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
+

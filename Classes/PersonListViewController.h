cfdbc4ed769b34255cc07b42150a9b0c5d2b5f7b
Takuya Murakami <tmurakam@tmurakam.org>
2010-05-06 19:45:33 +0900

    updated

---
+++ Classes/PersonListViewController.h
@@ -0,0 +1,16 @@
+//
+//  PersonListViewController.h
+//  lifecal
+//
+//  Created by 村上 卓弥 on 10/05/06.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PersonListViewController : UITableViewController {
+
+}
+
+@end

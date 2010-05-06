f9d1f13d2575673873828ec06b8d08648d054033
Takuya Murakami <tmurakam@tmurakam.org>
2010-05-06 19:02:45 +0900

    updated

--- Classes/Event.m
+++ Classes/Event.m
@@ -32,18 +32,18 @@ static NSMutableArray *events[3];
     NSMutableArray *ary = [[NSMutableArray alloc] init];
     events[type] = ary;
 
-#define EV(y, n) [Event addEvent:ary year:y name:n]
+#define EV(y, n) [Event _addEvent:ary year:(y) name:(n)]
 
     switch (type) {
-    case EV_AGE: // すべて数え年齢
-        EV(61, @"還暦");
-        EV(70, @"古稀");
-        EV(77, @"喜寿");
-        EV(80, @"傘寿");
-        EV(88, @"米寿");
-        EV(90, @"卆寿");
-        EV(99, @"白寿");
-        EV(100, @"百寿");
+    case EV_AGE: // すべて満年齢
+        EV(60, @"還暦");
+        EV(70-1, @"古稀");
+        EV(77-1, @"喜寿");
+        EV(80-1, @"傘寿");
+        EV(88-1, @"米寿");
+        EV(90-1, @"卆寿");
+        EV(99-1, @"白寿");
+        EV(100-1, @"百寿");
         break;
 
     case EV_MARRIAGE:
@@ -60,24 +60,24 @@ static NSMutableArray *events[3];
 
     case EV_DEATH:
         EV(1, @"一周忌");
-        EV(2, @"三回忌");
-        EV(6, @"七回忌");
-        EV(12, @"十三回忌");
-        EV(16, @"十七回忌");
-        EV(22, @"二十三回忌");
-        EV(26, @"二十七回忌");
-        EV(32, @"三十三回忌");
-        EV(49, @"五十回忌");
+        EV(3-1, @"三回忌");
+        EV(7-1, @"七回忌");
+        EV(13-1, @"十三回忌");
+        EV(17-1, @"十七回忌");
+        EV(23-1, @"二十三回忌");
+        EV(27-1, @"二十七回忌");
+        EV(33-1, @"三十三回忌");
+        EV(50-1, @"五十回忌");
         break;
     }
 
     return ary;
 }
 
-+ (void)_addEvent:(NSMutableArray*)ary year:(int)year name:(NSString *)name
++ (void)_addEvent:(NSMutableArray*)ary year:(int)years name:(NSString *)name
 {
     Event *ev = [[Event alloc] init];
-    ev.year = year;
+    ev.years = years;
     ev.name = name;
     [ary addObject:ev];
     [ev release];

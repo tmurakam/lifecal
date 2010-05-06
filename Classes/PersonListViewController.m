72693d9c14d7e3a48c6b4d97d4e5a91f86907597
Takuya Murakami <tmurakam@tmurakam.org>
2010-05-06 19:52:13 +0900

    updated

--- Classes/PersonListViewController.m
+++ Classes/PersonListViewController.m
@@ -24,7 +24,6 @@
 }
 */
 
-
 #pragma mark -
 #pragma mark View lifecycle
 
@@ -33,7 +32,7 @@
     [super viewDidLoad];
 
     // Uncomment the following line to preserve selection between presentations.
-    self.clearsSelectionOnViewWillAppear = NO;
+    //self.clearsSelectionOnViewWillAppear = NO;
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;

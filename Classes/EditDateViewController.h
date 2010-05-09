cc470758d09f499cd0555e79a3f4d57f59126443
Takuya Murakami <tmurakam@tmurakam.org>
2010-05-09 13:48:40 +0900

    add editdateview

---
+++ Classes/EditDateViewController.h
@@ -0,0 +1,29 @@
+//
+//  EditDateViewController.h
+//  lifecal
+//
+//  Created by 村上 卓弥 on 10/05/09.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class EditDateViewController;
+
+@protocol EditDateViewDelegate
+- (void)editDateViewChanged:(EditDateViewController *)vc;
+@end
+
+@interface EditDateViewController : UIViewController {
+    IBOutlet UIDatePicker *UIdatePicker;
+    
+    id<EditDateViewDelegate> delegate;
+    NSDate *date;
+}
+
+@property(nonatomic,assign) id<EditDateViewDelegate> delegate;
+@property(nonatomic,retain) NSDate *date;
+
+- (IBAction)removeDateButtonTapped:(id)sender;
+
+@end
